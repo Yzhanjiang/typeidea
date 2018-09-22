89f95880710db808a6caa6c5e97f1efed2ca5bcc
zhan <251642766@qq.com>
2018-09-22 13:22:44 +0800

    系统初始化

--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -74,16 +74,7 @@ WSGI_APPLICATION = 'typeidea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'typeidea',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '192.168.88.135',
-        'PORT': '3306',
-    }
-}
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
