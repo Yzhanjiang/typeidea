25e4577bb2bcde56cef96fc878d3f23a2968cba6
zhan <251642766@qq.com>
2018-09-24 22:35:07 +0800

    func view

--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -126,9 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+print(STATIC_URL)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+print(STATICFILES_DIRS)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 
