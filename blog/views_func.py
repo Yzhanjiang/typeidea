b2d507c1a55d571b425f22b0e3c3a9e7ed7eae23
zhan <251642766@qq.com>
2018-09-24 11:45:54 +0800

    CBV

--- blog/views_func.py
+++ blog/views_func.py
@@ -4,114 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from  django.core.paginator import  Paginator,EmptyPage
 from django.db import connection
-from django.http import HttpResponse, Http404
-from  django.views.generic import ListView,DetailView
-
-
 # Create your views here.
-
+from django.http import HttpResponse, Http404
 from .models import Post,Tag,Category
 from  config.models import SideBar
 from  comment.models import Comment
-
-
-#CBV
-
-class CommonMixin:
-    def get_category_context(self):
-        categories = Category.objects.filter(status=1)
-        nav_cates = []
-        cates = []
-        for cate in categories:
-            if cate.is_nav:
-                nav_cates.append(cate)
-            else:
-                cates.append(cate)
-        return  {
-                'cav_cates':nav_cates,
-                'cates':cates,
-                 }
-
-    def get_side_bars(self):
-        return SideBar.objects.filter(status=1)
-
-    def get_context_data(self,*args,**kwargs):
-        side_bars = SideBar.objects.filter(status=1)
-        recently_posts = Post.objects.filter(status=1)[:10]
-        # hot_posts = Post.objects.filter(status=1).order_by('views')
-        recently_comments = Comment.objects.filter(status=1)[:10]
-
-        print(connection.queries)
-        kwargs = {
-            'side_bars': side_bars,
-            'recently_posts': recently_posts,
-            'recently_comments': recently_comments,
-        }
-        # context.update(extra_context)
-        # return context
-        kwargs.update(self.get_category_context())
-        print(kwargs)
-        return  super(CommonMixin,self).get_context_data(**kwargs)
-
-
-class BasePostsView(CommonMixin,ListView):
-    model = Post
-    template_name = 'blog/list.html'
-    context_object_name =  'posts'
-    paginate_by = 3
-    allow_empty = True
-
-
-class IndexView(BasePostsView):
-    # def get(self,requset,*args,**kwargs):
-        pass
-
-
-class CategoryView(BasePostsView):
-    def get_queryset(self):
-        qs = super(CategoryView,self).get_queryset()
-        cate_id = self.kwargs.get('category_id')
-        qs = qs.filter(category_id= cate_id)
-        return qs
-
-
-class TagView(BasePostsView):
-    def get_queryset(self):
-        tag_id = self.kwargs('tag')
-        try :
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            return  []
-
-        posts =  tag.posts.all()
-        return posts
-
-
-class PostView(CommonMixin,DetailView):
-    model = Post
-    template_name = 'blog/detail.html'
-    context_object_name = 'post'
-    print(context_object_name)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#FUNC
-
 
 def get_common_context():
     categories = Category.objects.filter(status=1)
@@ -191,6 +88,8 @@ def post_list(request,category_id=None,tag_id=None):
     common_context = get_common_context()
     context.update(common_context)
     return render(request,'blog/list.html',context=context)
+
+
 
 def post_detail(request,pk=None):
     try:
--- typeidea/urls.py
+++ typeidea/urls.py
@@ -19,16 +19,22 @@ from django.contrib import admin
 from  custom_site import custom_site
 
 
-from blog.views import  post_list,post_detail
+from blog.views import  post_list,post_detail,IndexView,CategoryView,TagView,PostView
+
 from config.views import links
 
 urlpatterns = [
-    url(r'^$',post_list,name="index"),
-    url(r'category/(?P<category_id>\d+)/$',post_list,name="category"),
-    url(r'^tag/(?P<tag_id>\d+)/$',post_list,name="tag"),
-    url(r'^post/(?P<pk>\d+)/$',post_detail,name="detail"),
+    # url(r'^$',post_list,name="index"),
+    # url(r'category/(?P<category_id>\d+)/$',post_list,name="category"),
+    # url(r'^tag/(?P<tag_id>\d+)/$',post_list,name="tag"),
+    # url(r'^post/(?P<pk>\d+)/$',post_detail,name="detail"),
     url(r'^links/$',links),
-
     url(r'^admin/', admin.site.urls),
-    # url(r'^custom_site/',custom_site.urls ),
+
+    #CBV
+    url(r'^$',IndexView.as_view(),name="index"),
+    url(r'category/(?P<category_id>\d+)/$',CategoryView.as_view(),name="category"),
+    url(r'^tag/(?P<tag_id>\d+)/$',TagView.as_view(),name="tag"),
+    url(r'^post/(?P<pk>\d+)/$',PostView.as_view(),name="detail"),
+
 ]
